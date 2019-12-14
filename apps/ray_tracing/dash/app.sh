becb97d221d8d0aaadd3193e7d3b715e881ecc30
Deepti Raghavan <deeptir@cs.stanford.edu>
2019-12-13 18:30:28 -0800

    updates

--- apps/ray_tracing/dash/app.sh
+++ apps/ray_tracing/dash/app.sh
@@ -1,2 +1,4 @@
 cat 1.INFO | grep "\[RAY\]" | head -n1 | cut -c 7- > rays.csv
 cat *.INFO | grep "\[RAY\]" | grep -v pathID | cut -c 7- >> rays.csv
+cat rays.csv | q -H -d, "SELECT * FROM - WHERE pathID = 328512"
+cat rays.csv | q -H -d, "SELECT MAX(timestamp), MAX(hop) FROM - GROUP BY pathID LIMIT 5"
