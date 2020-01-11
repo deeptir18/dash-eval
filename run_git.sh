4d8b0f3918e06cadfa6c3a80bada1cbc03b238ec
Deepti Raghavan <deeptir@cs.stanford.edu>
2020-01-11 01:02:16 -0800

    Running git

---
+++ run_git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source config_async.sh
+python3 run_tests.py results_async bash git 0
+python3 run_tests.py results_async dash git 0
+
+python3 run_tests.py results_async bash git 1
+python3 run_tests.py results_async dash git 1
+
+python3 run_tests.py results_async bash git 2
+python3 run_tests.py results_async dash git 2
+
+python3 run_tests.py results_async bash git 3
+python3 run_tests.py results_async dash git 3
+
+python3 run_tests.py results_async bash git 4
+python3 run_tests.py results_async dash git 4
+
+
+
+
