1014359eb7f5e8bd30f281e613ff676982e94d25
Deepti Raghavan <deeptir@cs.stanford.edu>
2020-05-04 21:07:38 -0700

    Ray tracing log to file

--- apps/ray_tracing/dash/app.sh
+++ apps/ray_tracing/dash/app.sh
@@ -1,3 +1,3 @@
 cat ray_tracing/dash/logs/1.INFO | grep "\[RAY\]" | head -n1 | cut -c 7- > ray_tracing/dash/logs/rays.csv
 cat ray_tracing/dash/logs/*.INFO | grep "\[RAY\]" | grep -v pathID | cut -c 7- >> ray_tracing/dash/logs/rays.csv
-cat ray_tracing/dash/logs/rays.csv | sed -n '/^590432,/p'
+cat ray_tracing/dash/logs/rays.csv | sed -n '/^590432,/p' > /home/deeptir/research/fs_project/dash_eval/dash_output.log
