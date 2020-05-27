7316fadaf957d8ab836e31a6f9b0bc5efb4129c7
Deepti Raghavan <deeptir@cs.stanford.edu>
2020-05-27 11:15:14 -0700

    distributed stuff

--- apps/distributed/dash/run.sh
+++ apps/distributed/dash/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pushd "$CLIENT_MOUNT_PARENT"
 echo $PWD
-$TIMECOMMAND $CLIENT_BINARY $DASH_EVAL/apps/distributed/dash/app.sh --mount_file $DASH_CLIENT_PATH/config/distributed.config --annotations_file $DASH_CLIENT_PATH/config/eval_annotations.txt --pwd $CLIENT_MOUNT_PARENT --tmpfile $CLIENT_TMP --tracing_level warn
+$TIMECOMMAND $CLIENT_BINARY $DASH_EVAL/apps/distributed/dash/app.sh --mount_file $DASH_CLIENT_PATH/config/distributed.config --annotations_file $DASH_CLIENT_PATH/config/eval_annotations.txt --pwd
+$CLIENT_MOUNT_PARENT --tmpfile $CLIENT_TMP
 popd
--- run_distributed.sh
+++ run_distributed.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
-source distributed_config.sh # sync configuration
-python run_distributed.py results_distributed_sync dash distributed 0
-python run_distributed.py results_distributed_sync bash distributed 0
+export SYNC_FOLDER="/home/deeptir/research/results/may27_sync"
+export ASYNC_FOLDER="/home/deeptir/research/results/may27_async"
+source /home/deeptir/research/fs_project/results/configs/distributed_config.sh # sync configuration
+python run_distributed.py $SYNC_FOLDER dash distributed 0
+python run_distributed.py $SYNC_FOLDER bash distributed 0
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
-python run_distributed.py results_distributed_async dash distributed 0
-python run_distributed.py results_distributed_async bash distributed 0
+python run_distributed.py $ASYNC_FOLDER dash distributed 0
+python run_distributed.py $ASYNC_FOLDER bash distributed 0
 
 export SERVER_FOLDER="nfs_deepti_sync" # OR ASYNC
-python run_distributed.py results_distributed_sync dash distributed 1
-python run_distributed.py results_distributed_sync bash distributed 1
+python run_distributed.py $SYNC_FOLDER dash distributed 1
+python run_distributed.py $SYNC_FOLDER bash distributed 1
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
-python run_distributed.py results_distributed_async dash distributed 1
-python run_distributed.py results_distributed_async bash distributed 1
+python run_distributed.py $ASYNC_FOLDER dash distributed 1
+python run_distributed.py $ASYNC_FOLDER bash distributed 1
 
-python run_distributed.py results_distributed_sync dash distributed 2
-python run_distributed.py results_distributed_sync bash distributed 2
+python run_distributed.py $SYNC_FOLDER dash distributed 2
+python run_distributed.py $SYNC_FOLDER bash distributed 2
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
-python run_distributed.py results_distributed_async dash distributed 2
-python run_distributed.py results_distributed_async bash distributed 2
+python run_distributed.py $ASYNC_FOLDER dash distributed 2
+python run_distributed.py $ASYNC_FOLDER bash distributed 2
 
 export SERVER_FOLDER="nfs_deepti_sync" # OR ASYNC
-python run_distributed.py results_distributed_sync dash distributed 3
-python run_distributed.py results_distributed_sync bash distributed 3
+python run_distributed.py $SYNC_FOLDER dash distributed 3
+python run_distributed.py $SYNC_FOLDER bash distributed 3
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
-python run_distributed.py results_distributed_async dash distributed 3
-python run_distributed.py results_distributed_async bash distributed 3
+python run_distributed.py $ASYNC_FOLDER dash distributed 3
+python run_distributed.py $ASYNC_FOLDER bash distributed 3
 
 export SERVER_FOLDER="nfs_deepti_sync" # OR ASYNC
-python run_distributed.py results_distributed_sync dash distributed 4
-python run_distributed.py results_distributed_sync bash distributed 4
+python run_distributed.py $SYNC_FOLDER dash distributed 4
+python run_distributed.py $SYNC_FOLDER bash distributed 4
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
-python run_distributed.py results_distributed_async dash distributed 4
-python run_distributed.py results_distributed_async bash distributed 4
+python run_distributed.py $ASYNC_FOLDER dash distributed 4
+python run_distributed.py $ASYNC_FOLDER bash distributed 4
 
