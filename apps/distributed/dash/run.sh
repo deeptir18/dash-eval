3edac8cbdfd3292283debbe0405d7497217f6866
Deepti Raghavan <deeptir@cs.stanford.edu>
2020-05-27 19:29:28 -0700

    fix distributed script

--- apps/distributed/dash/run.sh
+++ apps/distributed/dash/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pushd "$CLIENT_MOUNT_PARENT"
 echo $PWD
-$TIMECOMMAND $CLIENT_BINARY $DASH_EVAL/apps/distributed/dash/app.sh --mount_file $DASH_CLIENT_PATH/config/distributed.config --annotations_file $DASH_CLIENT_PATH/config/eval_annotations.txt --pwd
-$CLIENT_MOUNT_PARENT --tmpfile $CLIENT_TMP
+$TIMECOMMAND $CLIENT_BINARY $DASH_EVAL/apps/distributed/dash/app.sh --mount_file $DASH_CLIENT_PATH/config/distributed.config --annotations_file $DASH_CLIENT_PATH/config/eval_annotations.txt --pwd $CLIENT_MOUNT_PARENT --tmpfile $CLIENT_TMP
 popd
--- run_distributed.sh
+++ run_distributed.sh
@@ -2,7 +2,7 @@
 export SYNC_FOLDER="/home/deeptir/research/results/may27_sync"
 export ASYNC_FOLDER="/home/deeptir/research/results/may27_async"
 source /home/deeptir/research/fs_project/results/configs/distributed_config.sh # sync configuration
-python run_distributed.py $SYNC_FOLDER dash distributed 0
+#python run_distributed.py $SYNC_FOLDER dash distributed 0
 python run_distributed.py $SYNC_FOLDER bash distributed 0
 
 export SERVER_FOLDER="nfs_deepti_async" # OR ASYNC
